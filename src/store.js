a4d5ad69db2aff37daba9a22b16f4cee285b2905
Tony Bento <bentotony1010@yahoo.com>
2019-10-23 14:37:59 -0500

    set

---
+++ src/store.js
@@ -0,0 +1,16 @@
+const uuid = require('uuid/v4');
+
+const bookmarks = [ 
+    { id: uuid(),
+      title: 'Google',
+      url: 'https://www.google.com',
+      description: 'Where we find everything else',
+      rating: 4 },
+    { id: uuid(),
+      title: 'MDN',
+      url: 'https://developer.mozilla.org',
+      description: 'The only place to find web documentation',
+      rating: 5 },
+];
+
+module.exports = { bookmarks };
